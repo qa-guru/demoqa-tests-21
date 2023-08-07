b159e92ef02b5a627901e3660e68d29e001e584a
svasenkov <aanher@gmail.com>
2023-08-07 20:31:37 +0200

    added  random utils

--- src/test/java/com/demoqa/tests/RegistrationWithRandomUtilsTests.java
+++ src/test/java/com/demoqa/tests/RegistrationWithRandomUtilsTests.java
@@ -6,8 +6,7 @@ import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
-import static com.demoqa.utils.RandomUtils.getRandomEmail;
-import static com.demoqa.utils.RandomUtils.getRandomString;
+import static com.demoqa.utils.RandomUtils.*;
 
 public class RegistrationWithRandomUtilsTests extends TestBase {
 
@@ -15,7 +14,8 @@ public class RegistrationWithRandomUtilsTests extends TestBase {
     void successfulRegistrationTest() {
         String firstName = getRandomString(10),
                 lastName = getRandomString(10),
-                userEmail = getRandomEmail();
+                userEmail = getRandomEmail(),
+                gender = getRandomGender();
 
         open("/automation-practice-form");
         $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
@@ -25,7 +25,7 @@ public class RegistrationWithRandomUtilsTests extends TestBase {
         $("#firstName").setValue(firstName);
         $("#lastName").setValue(lastName);
         $("#userEmail").setValue(userEmail);
-        $("#genterWrapper").$(byText("Other")).click();
+        $("#genterWrapper").$(byText(gender)).click();
         $("#userNumber").setValue("1234567890");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("July");
@@ -44,6 +44,6 @@ public class RegistrationWithRandomUtilsTests extends TestBase {
         $(".modal-dialog").should(appear);
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
         $(".table-responsive").shouldHave(text(firstName), text(lastName),
-                text(userEmail), text("1234567890"));
+                text(userEmail), text("1234567890"), text(gender));
     }
 }
--- src/test/java/com/demoqa/utils/RandomUtils.java
+++ src/test/java/com/demoqa/utils/RandomUtils.java
@@ -1,6 +1,7 @@
 package com.demoqa.utils;
 
 import java.security.SecureRandom;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class RandomUtils {
 
@@ -8,6 +9,9 @@ public class RandomUtils {
         System.out.println(getRandomString(10));
         System.out.println(getRandomString(10));
         System.out.println(getRandomEmail());
+        System.out.println(getRandomInt(111, 99999999));
+        System.out.println(getRandomPhone());
+        System.out.println(getRandomGender());
     }
 
     public static String getRandomString(int len) {
@@ -26,5 +30,27 @@ public class RandomUtils {
 //        return getRandomString(15) + "@" + getRandomString(5) + "." + getRandomString(5);
 //        return String.format("%s@%s.%s", getRandomString(15),
 //                getRandomString(5), getRandomString(5));
+    }
+
+    public static int getRandomInt(int min, int max) {
+        return ThreadLocalRandom.current().nextInt(min, max + 1);
+    }
+
+    // +3 (263) 253 - 66 - 12
+    public static String getRandomPhone() {
+        return String.format("+%s (%s) %s - %s - %s", getRandomInt(1, 9), getRandomInt(111, 999),
+                getRandomInt(111, 999), getRandomInt(11, 99), getRandomInt(11, 99));
+    }
+
+    public static String getRandomGender() {
+        String[] genders = {"Male", "Female", "Other"};
+
+        return getRandomItemFromArray(genders);
+    }
+
+    public static String getRandomItemFromArray(String[] array) {
+        int index = getRandomInt(0, array.length - 1);
+
+        return array[index];
     }
 }
